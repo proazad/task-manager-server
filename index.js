6d8e412439a1406f354bd88589114606414c24d7
proazad <proazadhossain7595@gmail.com>
2023-12-24 16:01:12 +0300

    Task Delete API Complete

--- index.js
+++ index.js
@@ -100,6 +100,14 @@ async function run() {
         });
 
 
+        // Delete Single Task By ID 
+        app.delete("/task/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
